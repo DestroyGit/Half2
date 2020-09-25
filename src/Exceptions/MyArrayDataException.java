66ee05696ee00ace82cb5fce344156a237ea7578
maxim <estonmax@gmail.com>
2020-09-25 15:09:17 +0300

    Added code with my exceptions (USE THIS)

--- src/Exceptions/MyArrayDataException.java
+++ src/Exceptions/MyArrayDataException.java
@@ -1,4 +1,20 @@
 package Exceptions;
 
-public class MyArrayDataException {
+public class MyArrayDataException extends NumberFormatException{
+    private int i;
+    private int j;
+
+    public int getI() {
+        return i;
+    }
+
+    public int getJ() {
+        return j;
+    }
+
+    public MyArrayDataException(String s, int i, int j) {
+        super(s);
+        this.i = i;
+        this.j = j;
+    }
 }
